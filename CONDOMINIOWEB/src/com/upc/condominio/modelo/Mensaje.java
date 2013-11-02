aa119ef9c87ac338ebf76a85ec66cce41bf33361
llp.lozano@gmail.com <llp.lozano@gmail.com@a45a85c4-9269-b22e-5742-fe0592db18eb>
2013-11-02 09:44:16 +0000

    Lilder Lozano (demo)

---
+++ CONDOMINIOWEB/src/com/upc/condominio/modelo/Mensaje.java
@@ -0,0 +1,15 @@
+package com.upc.condominio.modelo;
+
+import java.sql.Date;
+
+public class Mensaje {
+
+	private int n_idMens;
+	private String c_conten;
+	private String c_titulo;
+	private Date d_fecPub;
+	
+	public Mensaje() {}
+	
+	
+}
