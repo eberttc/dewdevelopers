de660af3d9b2a76bb16779e6482d0ad4a0e9620a
omcr1905@gmail.com <omcr1905@gmail.com@a45a85c4-9269-b22e-5742-fe0592db18eb>
2013-11-06 10:34:24 +0000

    Implementacion de CuotaCore

---
+++ CONDOMINIOWEB/src/com/upc/condominio/negocio/CuotaCore.java
@@ -0,0 +1,125 @@
+package com.upc.condominio.negocio;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.upc.condominio.dao.CuotaDAO;
+import com.upc.condominio.exceptions.DAOExcepcion;
+import com.upc.condominio.modelo.Cuota;
+
+public class CuotaCore {
+
+	public String insertar(Cuota cuota) throws DAOExcepcion {
+		
+		String v_vReturn = "ERROR - Consulte con Administrador";
+		CuotaDAO cuotaDAO = new CuotaDAO();
+		
+		try{
+			
+			v_vReturn = buscar(cuota);
+			
+			if (v_vReturn.equals("OK")){
+				cuota = cuotaDAO.insertar(cuota);
+				v_vReturn = "Cuota Grabada exitosamente.";
+			}
+		}
+		catch (Exception e) {
+            e.printStackTrace();
+        }
+        return v_vReturn;
+        
+	}
+	
+	public String buscar(Cuota cuota) throws DAOExcepcion {
+		
+		String v_vReturn = "ERROR - Consulte con Administrador";
+		Cuota cuotaBuscada = null; 
+
+		try{
+			CuotaDAO cuotaDAO = new CuotaDAO();
+			cuotaBuscada = cuotaDAO.buscar(cuota);
+			
+			if (cuotaBuscada == null){
+				v_vReturn = "OK";
+			}else if (cuotaBuscada != null){
+				v_vReturn = "ERROR - Cuota ya se encuentra registrado para Periodo y Vivienda.";
+			}
+		}
+		catch (Exception e) {
+            e.printStackTrace();
+        }
+        return v_vReturn;
+	}
+	
+	public Cuota obtener(int idCuota) throws DAOExcepcion {
+		Cuota cuota = null;
+		try{
+			CuotaDAO cuotaDao = new CuotaDAO();
+			cuota = cuotaDao.obtener(idCuota);
+		}
+		catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cuota;
+	}
+	
+	public String actualizar(Cuota cuota) throws DAOExcepcion {
+		
+		String v_vReturn = "ERROR - Consulte con Administrador";
+		CuotaDAO cuotaDAO = new CuotaDAO();
+		
+		try{
+			Cuota cuotaAux = new Cuota ();
+			cuotaAux = obtener(cuotaAux.getN_IdCuot());	
+			
+			if ((cuota.getC_Period()==cuotaAux.getC_Period())
+					&&(cuota.getN_IdVivi()==cuotaAux.getN_IdVivi())){
+				v_vReturn = "OK";
+			}else{
+				v_vReturn = buscar(cuota);
+			} 
+			
+			if (v_vReturn.equals("OK")){
+				cuota = cuotaDAO.actualizar(cuota);
+				v_vReturn = "Cuota editada exitosamente.";
+			}
+			
+		}
+		catch (Exception e) {
+            e.printStackTrace();
+        }
+        return v_vReturn;
+        
+	}
+
+	public String eliminar(int idCuota) throws DAOExcepcion {
+		
+		String vReturn = "NO_OK";
+		try{
+			CuotaDAO cuotaDao = new CuotaDAO();
+			vReturn = cuotaDao.eliminar(idCuota);
+		}
+		catch (Exception e) {
+            e.printStackTrace();
+        }
+        return vReturn;
+		
+	}
+
+	public Collection<Cuota> listar() throws DAOExcepcion {
+		Collection<Cuota> listaCuota = new ArrayList<Cuota>();
+		try {
+			CuotaDAO cuotaDAO = new CuotaDAO();
+			listaCuota = cuotaDAO.listar();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return listaCuota;
+	}
+
+}
