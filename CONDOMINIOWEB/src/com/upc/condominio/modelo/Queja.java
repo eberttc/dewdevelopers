7eef6ef15bc8d3141074ca5097c3394399e6f56b
migroalv@gmail.com <migroalv@gmail.com@a45a85c4-9269-b22e-5742-fe0592db18eb>
2013-11-07 00:42:00 +0000

    Actualizacion de Quejas y Visitas

--- CONDOMINIOWEB/src/com/upc/condominio/modelo/Queja.java
+++ CONDOMINIOWEB/src/com/upc/condominio/modelo/Queja.java
@@ -1,5 +1,70 @@
 package com.upc.condominio.modelo;
 
+import java.sql.Date;
+
 public class Queja {
 
+	private int intIdQueja;
+	private int intIdResidente;
+	private String strTipoQueja;
+	private String strMotivoQueja;
+	private Date dFechaQueja;
+	private String strEstadoQueja;
+
+	public Queja() {
+		
+	}
+	
+	public Queja(int intIdQueja, int intIdResidente, String strTipoQueja,
+			String strMotivoQueja, Date dFechaQueja, String strEstadoQueja) {
+		
+		this.intIdQueja = intIdQueja;
+		this.intIdResidente = intIdResidente;
+		this.strTipoQueja = strTipoQueja;
+		this.strMotivoQueja = strMotivoQueja;
+		this.dFechaQueja = dFechaQueja;
+		this.strEstadoQueja = strEstadoQueja;
+	}
+	
+	public int getintIdQueja() {
+		return intIdQueja;
+	}
+	public void setintIdQueja(int intIdQueja) {
+		this.intIdQueja = intIdQueja;
+	}
+	public int getintIdResidente() {
+		return intIdResidente;
+	}
+	public void setintIdResidente(int intIdResidente) {
+		this.intIdResidente = intIdResidente;
+	}
+	
+	public String getstrTipoQueja() {
+		return strTipoQueja;
+	}
+	public void setstrTipoQueja(String strTipoQueja) {
+		this.strTipoQueja = strTipoQueja;
+	}
+
+	public String getstrMotivoQueja() {
+		return strMotivoQueja;
+	}
+	public void setstrMotivoQueja(String strMotivoQueja) {
+		this.strMotivoQueja = strMotivoQueja;
+	}
+
+	public Date getdFechaQueja() {
+		return dFechaQueja;
+	}
+	public void setdFechaQueja(Date dFechaQueja) {
+		this.dFechaQueja = dFechaQueja;
+	}
+
+	public String getstrEstadoQueja() {
+		return strEstadoQueja;
+	}
+	public void setstrEstadoQueja(String strEstadoQueja) {
+		this.strEstadoQueja = strEstadoQueja;
+	}
+	
 }
