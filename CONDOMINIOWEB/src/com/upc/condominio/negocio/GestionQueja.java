5e03f3e31005a77634ae6f1c0887445ce3492492
migroalv@gmail.com <migroalv@gmail.com@a45a85c4-9269-b22e-5742-fe0592db18eb>
2013-11-07 02:05:42 +0000

    GestionQueja

--- CONDOMINIOWEB/src/com/upc/condominio/negocio/GestionQueja.java
+++ CONDOMINIOWEB/src/com/upc/condominio/negocio/GestionQueja.java
@@ -1,21 +1,16 @@
 package com.upc.condominio.negocio;
 
 import java.sql.Date;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-
-
 import com.upc.condominio.dao.QuejaDAO;
 import com.upc.condominio.exceptions.DAOExcepcion;
 import com.upc.condominio.modelo.Queja;
-import com.upc.condominio.util.FormatoFecha;
+
 
 public class GestionQueja {
 
 	  
 		public void insertarQueja(int intIdResidente,String strTipoQueja, String strMotivoQueja, Date dFechaQueja, String strEstadoQueja)
+
 				throws DAOExcepcion{
 				
 		
@@ -32,5 +27,15 @@ public class GestionQueja {
 				quejaDao.insertar(queja);
 		
 		}
+		
+		public Queja obtenerQueja(int codigo) throws DAOExcepcion{
+			
+			QuejaDAO quejaDao=new  QuejaDAO();	
+			Queja queja=new Queja();
+			queja=quejaDao.obtener(5);
+			
+			return queja;
+		}
+
 	
 }
