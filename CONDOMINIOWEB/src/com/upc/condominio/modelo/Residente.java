f358a45dc3706da826a45a3d20fbfe8ab38e129c
palmendi86@gmail.com <palmendi86@gmail.com@a45a85c4-9269-b22e-5742-fe0592db18eb>
2013-11-08 06:12:40 +0000

    residente

--- CONDOMINIOWEB/src/com/upc/condominio/modelo/Residente.java
+++ CONDOMINIOWEB/src/com/upc/condominio/modelo/Residente.java
@@ -4,95 +4,99 @@ import java.util.Date;
 
 public class Residente {
 
-	private int N_CodRes;
-	private String C_NomRes;
-	private int C_TipDoc;
-	private Date D_FecNac;
-	private String C_Correo;
-	private String C_NumDoc;
-	private String C_Clave;
-	private int C_EstReg;
+	private int idResidente;
+	private String nombreResidente;
+	private int tipoDocumento;
+	private Date fechaNacimiento;
+	private String correo;
+	private String numeroDocumento;
+	private String clave;
+	private int estadoRegistro;
 	
 	public Residente() {
 	}
 
-	public Residente(int n_CodRes, String c_NomRes, int c_TipDoc,
-			Date d_FecNac, String c_Correo, String c_NumDoc, String c_Clave,
-			int c_EstReg) {
+		
+	public Residente(int idResidente, String nombreResidente,
+			int tipoDocumento, Date fechaNacimiento, String correo,
+			String numeroDocumento, String clave, int estadoRegistro) {
 		super();
-		N_CodRes = n_CodRes;
-		C_NomRes = c_NomRes;
-		C_TipDoc = c_TipDoc;
-		D_FecNac = d_FecNac;
-		C_Correo = c_Correo;
-		C_NumDoc = c_NumDoc;
-		C_Clave = c_Clave;
-		C_EstReg = c_EstReg;
+		this.idResidente = idResidente;
+		this.nombreResidente = nombreResidente;
+		this.tipoDocumento = tipoDocumento;
+		this.fechaNacimiento = fechaNacimiento;
+		this.correo = correo;
+		this.numeroDocumento = numeroDocumento;
+		this.clave = clave;
+		this.estadoRegistro = estadoRegistro;
 	}
 
-	public int getN_CodRes() {
-		return N_CodRes;
+
+
+	public int getIdResidente() {
+		return idResidente;
 	}
 
-	public void setN_CodRes(int n_CodRes) {
-		N_CodRes = n_CodRes;
+	public void setIdResidente(int idResidente) {
+		this.idResidente = idResidente;
 	}
 
-	public String getC_NomRes() {
-		return C_NomRes;
+	public String getNombreResidente() {
+		return nombreResidente;
 	}
 
-	public void setC_NomRes(String c_NomRes) {
-		C_NomRes = c_NomRes;
+	public void setNombreResidente(String nombreResidente) {
+		this.nombreResidente = nombreResidente;
 	}
 
-	public int getC_TipDoc() {
-		return C_TipDoc;
+	public int getTipoDocumento() {
+		return tipoDocumento;
 	}
 
-	public void setC_TipDoc(int c_TipDoc) {
-		C_TipDoc = c_TipDoc;
+	public void setTipoDocumento(int tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
 	}
 
-	public Date getD_FecNac() {
-		return D_FecNac;
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
 	}
 
-	public void setD_FecNac(Date d_FecNac) {
-		D_FecNac = d_FecNac;
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
 	}
 
-	public String getC_Correo() {
-		return C_Correo;
+	public String getCorreo() {
+		return correo;
 	}
 
-	public void setC_Correo(String c_Correo) {
-		C_Correo = c_Correo;
+	public void setCorreo(String correo) {
+		this.correo = correo;
 	}
 
-	public String getC_NumDoc() {
-		return C_NumDoc;
+	public String getNumeroDocumento() {
+		return numeroDocumento;
 	}
 
-	public void setC_NumDoc(String c_NumDoc) {
-		C_NumDoc = c_NumDoc;
+	public void setNumeroDocumento(String numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
 	}
 
-	public String getC_Clave() {
-		return C_Clave;
+	public String getClave() {
+		return clave;
 	}
 
-	public void setC_Clave(String c_Clave) {
-		C_Clave = c_Clave;
+	public void setClave(String clave) {
+		this.clave = clave;
 	}
 
-	public int getC_EstReg() {
-		return C_EstReg;
+	public int getEstadoRegistro() {
+		return estadoRegistro;
 	}
 
-	public void setC_EstReg(int c_EstReg) {
-		C_EstReg = c_EstReg;
+	public void setEstadoRegistro(int estadoRegistro) {
+		this.estadoRegistro = estadoRegistro;
 	}
+
 
 	
 	
