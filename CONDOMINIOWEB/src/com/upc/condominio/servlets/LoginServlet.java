03abe136efa6bcdc0141cf56a853422eb1325fef
eberttc@gmail.com <eberttc@gmail.com@a45a85c4-9269-b22e-5742-fe0592db18eb>
2013-11-28 04:24:59 +0000

    login momentaneo

---
+++ CONDOMINIOWEB/src/com/upc/condominio/servlets/LoginServlet.java
@@ -0,0 +1,59 @@
+package com.upc.condominio.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ * Servlet implementation class for Servlet: LoginServlet
+ * 
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends javax.servlet.http.HttpServlet implements
+		javax.servlet.Servlet {
+	static final long serialVersionUID = 1L;
+
+	/*
+	 * (non-Java-doc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+	}
+
+	/*
+	 * (non-Java-doc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request,
+	 *      HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/*
+	 * (non-Java-doc)
+	 * 
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request,
+	 *      HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		String usuario = request.getParameter("usuario");
+		String clave = request.getParameter("clave");
+
+	
+
+		RequestDispatcher rd = request.getRequestDispatcher("/pages/principal.jsp");
+		rd.forward(request, response);
+	}
+}
