c2f38fc2553db2358eee4d3b54d6562acafc1a1a
palmendi86@gmail.com <palmendi86@gmail.com@a45a85c4-9269-b22e-5742-fe0592db18eb>
2013-11-30 15:09:04 +0000

    servlet residente

---
+++ trunk/SISTEMACONDOMINIOWEB/src/com/upc/condominio/servlets/ResidenteServlet.java
@@ -0,0 +1,162 @@
+package com.upc.condominio.servlets;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.upc.condominio.exceptions.DAOExcepcion;
+import com.upc.condominio.modelo.Residente;
+import com.upc.condominio.negocio.GestionResidente;
+
+/**
+ * Servlet implementation class ResidenteServlet
+ */
+@WebServlet("/ResidenteServlet")
+public class ResidenteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ResidenteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		//Variables del servlet
+				String param = request.getParameter("Param");
+				String idResidente = request.getParameter("id");
+				String paginaDestino = "";
+
+				
+				//Variables del negocio
+				GestionResidente residenteCore = new GestionResidente();
+						
+				try {
+					if (param.equals("eliminar")) {
+						
+				        residenteCore.eliminar(Integer.parseInt(idResidente));
+				        paginaDestino = "/pages/ResidenteBuscar.jsp";
+					
+					}else if (param.equals("editar")) {
+						
+		                Residente residente = residenteCore.obtener(Integer.parseInt(idResidente));
+		                request.setAttribute("residente", residente);
+		                
+		                paginaDestino = "/pages/ResidenteEditar.jsp";
+					}else
+						paginaDestino = "/pages/ResidenteBuscar.jsp";
+					
+					
+					RequestDispatcher rd = request.getRequestDispatcher(paginaDestino);
+		        	rd.forward(request, response);
+		        	
+				}catch (DAOExcepcion e) {
+		            System.out.println(e.getMessage());
+		            RequestDispatcher rd = request.getRequestDispatcher("error.jsp");
+		            rd.forward(request, response);
+				}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Variables del servlet
+				String param = request.getParameter("Param");
+				String nombre = request.getParameter("nombre");
+				String paginaDestino = "";
+				String vReturn = "";
+				SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+
+				//Variables con datos de la página
+				String idResidente = request.getParameter("txtID");
+				String nombreResidente = request.getParameter("txtNombre");
+				String tipoDocumento = request.getParameter("txtTipoDocumento");
+				String fechaNacimiento = request.getParameter("txtFeNac");
+				String correo = request.getParameter("txtCorreo");
+				String numeroDocumento = request.getParameter("txtNuDocumento");
+				//String clave = request.getParameter("clave");
+				
+				//Variables del negocio
+				GestionResidente residenteCore = new GestionResidente();
+								
+				try {
+					if (param.equals("nuevo")) { //cargar los combos
+						
+				        //List<Local> listaLocal = localCore.listarLocal();
+		                //request.setAttribute("locales", listaLocal);
+		                
+		                paginaDestino = "/pages/ResidenteNuevo.jsp";
+						
+					}else if (param.equals("insertar")) {
+
+						Residente residente = new Residente();
+						residente.setNombreResidente(nombreResidente);
+						residente.setTipoDocumento(Integer.parseInt(tipoDocumento));
+						residente.setFechaNacimiento(df.parse(fechaNacimiento));
+						residente.setCorreo(correo);
+						residente.setNumeroDocumento(numeroDocumento);
+						//residente.setClave(clave);
+										
+						vReturn = residenteCore.insertar(residente);
+						
+						paginaDestino = "/pages/Satisfactorio.jsp";
+						
+					}else if (param.equals("buscar")) {
+						
+						List<Residente> listaResidente = residenteCore.buscarPorNombre(nombre);
+		                request.setAttribute("residentes", listaResidente);
+		                                
+		                paginaDestino = "/pages/ResidenteBuscar.jsp";
+		                
+					}else if (param.equals("modificar")) {
+
+						Residente residente = new Residente();
+						residente.setIdResidente(Integer.parseInt(idResidente));
+						residente.setNombreResidente(nombreResidente);
+						residente.setTipoDocumento(Integer.parseInt(tipoDocumento));
+						residente.setFechaNacimiento(df.parse(fechaNacimiento));
+						residente.setCorreo(correo);
+						residente.setNumeroDocumento(numeroDocumento);
+						//residente.setClave(clave);
+						
+						vReturn = residenteCore.actualizar(residente);
+						
+						paginaDestino = "/pages/Satisfactorio.jsp";
+					}else
+						paginaDestino = "/pages/ResidenteBuscar.jsp";
+					
+					RequestDispatcher rd = request.getRequestDispatcher(paginaDestino);
+		        	rd.forward(request, response);
+		        	
+				}catch (DAOExcepcion e) {
+		            System.out.println(e.getMessage());
+		            RequestDispatcher rd = request.getRequestDispatcher("/pages/error.jsp");
+		            rd.forward(request, response);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+		
+		
+	}
+
+}
