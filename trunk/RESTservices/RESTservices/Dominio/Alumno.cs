3d9e1960503200b362663ae460e960ac6cdb965d
eberttc@gmail.com <eberttc@gmail.com@a45a85c4-9269-b22e-5742-fe0592db18eb>
2014-09-23 04:46:04 +0000

    ok

---
+++ trunk/RESTservices/RESTservices/Dominio/Alumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace RESTservices.Dominio
+{
+    [DataContract]
+    public class Alumno
+    {
+
+        [DataMember]
+        public string codigo { get; set; }
+
+        [DataMember]
+        public string nombre { get; set; }
+
+    }
+}
---
+++ trunk/RESTservices/RESTservices/Persistencia/ConexionUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RESTservices.Persistencia
+{
+    public class ConexionUtil
+    {
+
+        public static String cadena { 
+            get {
+
+
+                return "DataSource=(local);"+
+                       "Initial catalog=Votacion;"+
+                       "Integrated Security=SSPI;";
+
+            }
+        }
+    }
+}
